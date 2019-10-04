841ae4590097d90d63b05ab190095a6064c5ef9d
Yuka <42938128+Yukashimi@users.noreply.github.com>
2019-10-04 17:49:02 -0300

    Create Locale API
    EN: Updated the api.js and the app.js with the new routes to deal with the locales used in the app.
    
    PT: Atualizei o api.js e o app.js com as novas rotas que manuseiam os locais utilizados pelo app.
    
    Routes:
    DELETE ``/locais``
    GET ``/locais``
    PUT ``/locais``
    POST ``/locais``

--- api.js
+++ api.js
@@ -1,4 +1,4 @@
-const sql = require("mssql");
+const mysql = require("promise-mysql");
 const crypto = require("crypto");
 
 let connection;
@@ -10,21 +10,120 @@ const settings = {
   database: process.env.DB_NAME
 };
 
+function deleteLocais(req, res){
+  const id = req.query.id;
+  mysql.createConnection(settings)
+  .then((pool) => {
+    connection = pool;
+    return connection.query(`DELETE FROM TbLocal WHERE idLocal = ${id}`);
+  })
+  .then((results) => {
+    console.log(results);
+    //do other stuff maybe
+    res.writeHead(200, {"Content-Type": "application/json"});
+    res.end(JSON.stringify({"msg": "local deletado com sucesso?"}));
+  });
+}
+
+function getLocais(req, res){
+  mysql.createConnection(settings)
+  .then((pool) => {
+    connection = pool;
+    return connection.query("SELECT * FROM TbLocal");
+  })
+  .then((rows) => {
+    let info_locais = [];
+    for(let i = 0; i > rows.length; i++){
+      info_locais = {
+        id: rows[i].idLocal,
+        nome: rows[i].nome,
+        descricao: rows[i].descricao,
+        link: rows[i].link,
+        longitude: rows[i].longitude,
+        latitude: rows[i].latitude,
+        pontos: rows[i].pontos,
+        dataCadastro: rows[i].dataCadastro
+      };
+    }
+    
+    res.writeHead(200, {"Content-Type": "application/json"});
+    res.end(JSON.stringify({"info": info_locais}));
+  });
+}
+
 function login(req, res){
   const user = req.body.user;
   const pass = req.body.pass;
   
-  new sql.ConnectionPool(settings).connect()
+  mysql.createConnection(settings)
   .then((pool) => {
     connection = pool;
-    return connection.request().query(`SELECT nome, senha FROM TbUsuario WHERE noem = ${user}`);
+    return connection.query(`SELECT senha FROM TbUsuario WHERE email = ${user}`);
   })
   .then((rows) => {
-    const isLogged = (hasher(pass) === rows.recordset[0].senha);
-	const statusToSend = isLogged ? 200 : 403;
+    const isLogged = (hasher(pass) === rows[0].senha);
+	  const statusToSend = isLogged ? 200 : 403;
 
-	res.writeHead(statusToSend, {"Content-Type": "application/json"});
+	  res.writeHead(statusToSend, {"Content-Type": "application/json"});
     res.end(JSON.stringify({"isLogged": isLogged}));
+  });
+}
+
+function setLocais(req, res){
+  const info = req.body.info;
+  const now = new Date();
+  
+  mysql.createConnection(settings)
+  .then((pool) => {
+    connection = pool;
+    return connection.query(`INSERT INTO TbLocal (nome, descricao, link, longitude, latitude, pontos, dataCadastro) VALUES ('${info.nome}', '${info.descricao}', '${info.link}', '${info.longitude}', '${info.latitude}', 10, '${now}')`);
+  })
+  .then((results) => {
+    console.log(results);
+    //do other stuff maybe
+    res.writeHead(200, {"Content-Type": "application/json"});
+    res.end(JSON.stringify({"msg": "local inserido com sucesso?"}));
+  });
+}
+
+function setUser(req, res){
+  const info = req.body.info;
+  const now = new Date();
+  
+  if(info.senha !== info.senhaConfirmacao){
+    res.writeHead(400, {"Content-Type": "application/json"});
+    res.end(JSON.stringify({"msg": "senhas não conferem"}));
+    return null;
+  }
+  
+  mysql.createConnection(settings)
+  .then((pool) => {
+    connection = pool;
+    const senhaHasheada = hasher(info.senha);
+    return connection.query(`INSERT INTO TbUsuario (nome, sobreNome, email, senha, idNivelAcesso, qtdPontos, dataCadastro) VALUES ('${info.nome}', '${info.sobreNome}', '${info.email}', '${senhaHasheada}'), 1, 0, '${now}'`);
+  })
+  .then((results) => {
+    console.log(results);
+    //do other stuff maybe
+    res.writeHead(200, {"Content-Type": "application/json"});
+    res.end(JSON.stringify({"msg": "usuário inserido com sucesso?"}));
+  });
+}
+
+function updateLocais(req, res){
+  const info = req.body.info;
+  const update = `UPDATE TbLocal SET nome = '${info.nome}', descricao = '${info.descricao}', link = '${info.link}', longitude = '${info.longitude}', latitude = '${info.latitude}' WHERE idLocal = info.id`;
+  
+  mysql.createConnection(settings)
+  .then((pool) => {
+    connection = pool;
+    return connection.query(update);
+  })
+  .then((results) => {
+    console.log(results);
+    //do other stuff maybe
+    res.writeHead(200, {"Content-Type": "application/json"});
+    res.end(JSON.stringify({"msg": "informações atualizadas com sucesso?"}));
   });
 }
 
@@ -33,5 +132,8 @@ function hasher(plainText=""){
 }
 
 module.exports = {
-  login: login
+  getLocais: getLocais,
+  login: login,
+  setLocais: setLocais,
+  updateLocais: updateLocais
 };
--- app.js
+++ app.js
@@ -10,12 +10,12 @@ const api = require("./api.js");
 app.use(express.static('./public')); // load UI from public folder
 app.use(bodyParser.json());
 
+app.put("/user", (req, res) => api.setUser(req, res));
 app.post("/login", (req, res) => api.login(req, res));
 
+app.delete("/locais", (req, res) => api.deleteLocais(req, res));
+app.get("/locais", (req, res) => api.getLocais(req, res));
+app.put("/locais", (req, res) => api.setLocais(req, res));
+app.post("/locais", (req, res) => api.updateLocais(req, res));
+
 module.exports = app;
-
-EN: This is the initial commit, it only brings to the table an example of how to setup an express server, it also has a POST route ``/login``, the login function doesn't have anything fancy just yet, it just returns true of false depending if the user's password matches or not.PT: Primeiro commit. Nada de muito especial, apenas o exemplo de server utilizando express, alem de uma rota para login: ``/login``, ela nao faz nada de muito avancado, apenas retorna true ou false depedendo de se a senha do usuario estiver correta.
-
-[00:31]
-Heduardo:
-	PT: Primeiro commit. Nada de muito especial, apenas o exemplo de server utilizando express, alem de uma rota para login: ``/login``, ela não faz nada de muito avançado, apenas retorna true ou false dependendo de se a senha do usuário estiver correta.
