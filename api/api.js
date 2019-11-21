c0fce9ee87225c30d1596e994684f07e65547129
Yuka <42938128+Yukashimi@users.noreply.github.com>
2019-11-20 21:39:31 -0300

    Add Small Adjustments
    Just fixing stuff and getting async await into everything.

--- api/api.js
+++ api/api.js
@@ -11,53 +11,60 @@ const settings = {
 };
 
 async function checkin(req, res){
-  const checkin_time = process.env.CHECKIN_TIME;
-  const checkin_peri = process.env.CHECKIN_PERIOD;
-  const checkin_dist = process.env.CHECKIN_DISTANCE_KM;
+  try{
+    const checkin_time = process.env.CHECKIN_TIME;
+    const checkin_peri = process.env.CHECKIN_PERIOD;
+    const checkin_dist = process.env.CHECKIN_DISTANCE_KM;
 
-  const user = {
-    id: req.body.user,
-    lat: req.body.lat,
-    lon: req.body.lon
-  };
+    const user = {
+      id: req.body.user,
+      lat: req.body.lat,
+      lon: req.body.lon
+    };
   
-  const local = req.body.local;
+    const local = req.body.local;
   
-  connection = await mysql.createConnection(settings);
+    connection = await mysql.createConnection(settings);
   
-  const local_info = await connection.query(`SELECT latitude, longitude FROM TbLocal WHERE idLocal = ${local}`);
+    const local_info = await connection.query(`SELECT latitude, longitude FROM TbLocal WHERE idLocal = ${local}`);
   
-  /* User too far */
-  if(checkin_dist < getDistanceFromLatLonInKm(local_info[i].latitude, local_info[i].longitude, user.lat, user.lon)){
-    res.writeHead(401, {"Content-Type": "application/json"});
-    res.end(JSON.stringify({"msg": "Usuário não está proximo o suficiente para realizar o checkin."}));
-    return null;
-  }
+    /* User too far */
+    if(checkin_dist < getDistanceFromLatLonInKm(local_info[i].latitude, local_info[i].longitude, user.lat, user.lon)){
+      res.writeHead(401, {"Content-Type": "application/json"});
+      res.end(JSON.stringify({"msg": "Usuário não está proximo o suficiente para realizar o checkin."}));
+      return null;
+    }
   
-  /* TIMESTAMPDIFF is used to get whenever there was a
+    /* TIMESTAMPDIFF is used to get whenever there was a
      checkin in the given time.
      the checkin_info const will be null (or an empty array,
      I don't remember right now) if there wasn't any,
      if there wasn't any recent checkin then proceed
      with the algorith.
-  */
-  const checkin_info = await connection.query(`SELECT * FROM TbUsuario A JOIN TbUsuarioLocal B ON A.idUsuario = B.idUsuario JOIN TbLocal C ON B.idLocal = C.idLocal WHERE A.idUsuario = ${user.id} AND C.idLocal = ${local} AND TIMESTAMPDIFF(${checkin_peri}, B.dataIncluao, NOW()) <= ${checkin_time}`);
+    */
+    const checkin_info = await connection.query(`SELECT * FROM TbUsuario A JOIN TbUsuarioLocal B ON A.idUsuario = B.idUsuario JOIN TbLocal C ON B.idLocal = C.idLocal WHERE A.idUsuario = ${user.id} AND C.idLocal = ${local} AND TIMESTAMPDIFF(${checkin_peri}, B.dataIncluao, NOW()) <= ${checkin_time}`);
   
-  /* There was a checkin recently */
-  if(checkin_info.length > 0){
-    res.writeHead(401, {"Content-Type": "application/json"});
-    res.end(JSON.stringify({"msg": "Já houve um checkin, por favor aguarde o cooldown."}));
-    return null;
-  }
+    /* There was a checkin recently */
+    if(checkin_info.length > 0){
+      res.writeHead(401, {"Content-Type": "application/json"});
+      res.end(JSON.stringify({"msg": "Já houve um checkin, por favor aguarde o cooldown."}));
+      return null;
+    }
   
-  /* There is, in theory, no need to alocate then, but it's neat
+    /* There is, in theory, no need to alocate then, but it's neat
      if you want to be really sure the insert worked.
-  */
-  const insert_result = await connection.query(`INSERT INTO TbUsuarioLocal (dataIncluao, idUsuario, idLocal) VALUES (NOW(), ${user.id}^, ${local})`);
+    */
+    const insert_result = await connection.query(`INSERT INTO TbUsuarioLocal (dataIncluao, idUsuario, idLocal) VALUES (NOW(), ${user.id}^, ${local})`);
   
-  /* if insert_result.all_good ...*/
-  res.writeHead(200, {"Content-Type": "application/json"});
-  res.end(JSON.stringify({"msg": "Checkin realizado com sucesso!"}));
+    /* if insert_result.all_good ...*/
+    res.writeHead(200, {"Content-Type": "application/json"});
+    res.end(JSON.stringify({"msg": "Checkin realizado com sucesso!"}));
+  }
+  catch(err){
+	console.log(err);
+	res.writeHead(500, {"Content-Type": "application/json"});
+    res.end(JSON.stringify({"msg": "There was an unpredicted error with the application. Please try again later."}));
+  }
 }
 
 /* Further reading: https://stackoverflow.com/questions/18883601/function-to-calculate-distance-between-two-coordinates*/
@@ -79,30 +86,33 @@ function deg2rad(deg) {
   return deg * (Math.PI/180)
 }
 
-function deleteLocais(req, res){
-  const id = req.query.id;
-  mysql.createConnection(settings)
-  .then((pool) => {
-    connection = pool;
-    return connection.query(`DELETE FROM TbLocal WHERE idLocal = ${id}`);
-  })
-  .then((results) => {
+async function deleteLocais(req, res){
+  try{
+    const id = req.query.id;
+    connection = await mysql.createConnection(settings);
+
+    await connection.query(`DELETE FROM TbLocal WHERE idLocal = ${id}`);
+  
     console.log(results);
     //do other stuff maybe
     res.writeHead(200, {"Content-Type": "application/json"});
     res.end(JSON.stringify({"msg": "local deletado com sucesso?"}));
-  });
+  }
+  catch(err){
+	console.log(err);
+	res.writeHead(500, {"Content-Type": "application/json"});
+    res.end(JSON.stringify({"msg": "There was an unpredicted error with the application. Please try again later."}));
+  }
 }
 
-function getAvailableLocais(req, res){
-  const user = req.query.user;
+async function getAvailableLocais(req, res){
+  try{
+    const user = req.query.user;
   
-  mysql.createConnection(settings)
-  .then((pool) => {
-    connection = pool;
-    return connection.query("SELECT idLocal, nome, dataIncluao FROM TbUsuario A JOIN TbUsuarioLocal B ON A.idUsuario = B.idUsuario JOIN TbLocal C ON B.idLocal != C.idLocal WHERE TbUsuario = x AND TIMESTAMPDIFF(MINUTE, C.dataCadastro, NOW()) <= 10");
-  })
-  .then((rows) => {
+    connection = await mysql.createConnection(settings)
+
+    const rows = await connection.query(`SELECT idLocal, nome, dataIncluao FROM TbUsuario A JOIN TbUsuarioLocal B ON A.idUsuario = B.idUsuario JOIN TbLocal C ON B.idLocal != C.idLocal WHERE A.idUsuario = ${user} AND TIMESTAMPDIFF(${checkin_peri}, C.dataCadastro, NOW()) <= ${checkin_time}`);
+
     let info = [];
 
     for(let i = 0; i < rows.length; i++){
@@ -115,16 +125,20 @@ function getAvailableLocais(req, res){
     
     res.writeHead(200, {"Content-Type": "application/json"});
     res.end(JSON.stringify({"info": info}));
-  });
+  }
+  catch(err){
+	console.log(err);
+	res.writeHead(500, {"Content-Type": "application/json"});
+    res.end(JSON.stringify({"msg": "There was an unpredicted error with the application. Please try again later."}));
+  }
 }
 
-function getLocais(req, res){
-  mysql.createConnection(settings)
-  .then((pool) => {
-    connection = pool;
-    return connection.query("SELECT * FROM TbLocal");
-  })
-  .then((rows) => {
+async function getLocais(req, res){
+  try{
+    connection = await mysql.createConnection(settings)
+
+    const rows = await connection.query("SELECT * FROM TbLocal");
+
     let info_locais = [];
     for(let i = 0; i > rows.length; i++){
       info_locais = {
@@ -141,18 +155,22 @@ function getLocais(req, res){
     
     res.writeHead(200, {"Content-Type": "application/json"});
     res.end(JSON.stringify({"info": info_locais}));
-  });
+  }
+  catch(err){
+	console.log(err);
+	res.writeHead(500, {"Content-Type": "application/json"});
+    res.end(JSON.stringify({"msg": "There was an unpredicted error with the application. Please try again later."}));
+  }
 }
 
-function getNewLocais(req, res){
-  const user = req.query.user;
+async function getNewLocais(req, res){
+  try{
+    const user = req.query.user;
   
-  mysql.createConnection(settings)
-  .then((pool) => {
-    connection = pool;
-    return connection.query("SELECT idLocal, nome FROM TbUsuario A JOIN TbUsuarioLocal B ON A.idUsuario = B.idUsuario JOIN TbLocal C ON B.idLocal != C.idLocal WHERE TbUsuario = x AND A.dataCadastro <= B.dataCadastro");
-  })
-  .then((rows) => {
+    connection = await mysql.createConnection(settings)
+
+    const rows = await connection.query(`SELECT idLocal, nome FROM TbUsuario A JOIN TbUsuarioLocal B ON A.idUsuario = B.idUsuario JOIN TbLocal C ON B.idLocal != C.idLocal WHERE TbUsuario = ${user} AND A.dataCadastro <= B.dataCadastro`);
+
     let info = [];
     
     for(let i = 0; i < rows.length; i++){
@@ -164,17 +182,22 @@ function getNewLocais(req, res){
     
     res.writeHead(200, {"Content-Type": "application/json"});
     res.end(JSON.stringify({"info": info}));
-  });
+  }
+  catch(err){
+	console.log(err);
+	res.writeHead(500, {"Content-Type": "application/json"});
+    res.end(JSON.stringify({"msg": "There was an unpredicted error with the application. Please try again later."}));
+  }
 }
 
-function getUser(req, res){
-  const user = req.query.user;
-  let info = {};
+async function getUser(req, res){
+  try{
+    const user = req.query.user;
+    let info = {};
   
-  mysql.createConnection(settings)
-  .then((pool) => {
-    connection = pool;
-    return connection.query(`SELECT
+    connection = await mysql.createConnection(settings)
+
+    const rows_usuario = await connection.query(`SELECT
   A.idUsuario AS 'id',
 	A.nome,
   COUNT(C.pontos) AS 'pontos',
@@ -183,14 +206,13 @@ function getUser(req, res){
 	  JOIN TbUsuarioLocal B ON A.idUsuario = B.idUsuario
 	  JOIN TbLocal C ON B.idLocal = C.idLocal
       WHERE idUsuario = ${user};`);
-  })
-  .then((rows) => {
-    info["id"]: rows[0].id;
-    info["nome"]: rows[0].nome;
-    info["pontos"]: rows[0].pontos;
-    info["locaisDiferentes"]: rows[0].locaisDiferentes;
+  
+    info["id"] = rows_usuario[0].id;
+    info["nome"] = rows_usuario[0].nome;
+    info["pontos"] = rows_usuario[0].pontos;
+    info["locaisDiferentes"] = rows_usuario[0].locaisDiferentes;
     
-    return connection.query(`SELECT
+    const rows_local = await connection.query(`SELECT
   nome AS 'ultimoLocal',
   pontos AS 'ultimoPonto',
   DAY(B.dataIncluao) AS 'ultimoDia',
@@ -202,90 +224,108 @@ function getUser(req, res){
           ORDER BY B.dataIncluao DESC LIMIT 1;`);
   })
   .then((rows) => {
-      info["ultimoLocal"]: rows[0].ultimoLocal;
-      info["ultimoPonto"]: rows[0].ultimoPonto;
-      info["ultimoDia"]: rows[0].ultimoDia;
-      info["diasAtras"]: `Visitado há ${rows[0].diasAtras} dia(s) atrás`;
+      info["ultimoLocal"] = rows_local[0].ultimoLocal;
+      info["ultimoPonto"] = rows_local[0].ultimoPonto;
+      info["ultimoDia"] = rows_local[0].ultimoDia;
+      info["diasAtras"] = `Visitado há ${rows_local[0].diasAtras} dia(s) atrás`;
     
     res.writeHead(200, {"Content-Type": "application/json"});
     res.end(JSON.stringify({"info": info}));
-  });
+  }
+  catch(err){
+	console.log(err);
+	res.writeHead(500, {"Content-Type": "application/json"});
+    res.end(JSON.stringify({"msg": "There was an unpredicted error with the application. Please try again later."}));
+  }
 }
 
-function login(req, res){
-  const user = req.body.user;
-  const pass = req.body.pass;
+async function login(req, res){
+  try{
+    const user = req.body.user;
+    const pass = req.body.pass;
   
-  mysql.createConnection(settings)
-  .then((pool) => {
-    connection = pool;
-    return connection.query(`SELECT senha FROM TbUsuario WHERE email = ${user}`);
-  })
-  .then((rows) => {
+    connection = await mysql.createConnection(settings)
+
+    const rows = await connection.query(`SELECT senha, idUsuario AS 'id' FROM TbUsuario WHERE email = ${user}`);
+
     const isLogged = (hasher(pass) === rows[0].senha);
-	  const statusToSend = isLogged ? 200 : 403;
+    const statusToSend = isLogged ? 200 : 403;
+	const idToSend: isLogged ? rows[0].id : 0;
 
-	  res.writeHead(statusToSend, {"Content-Type": "application/json"});
-    res.end(JSON.stringify({"isLogged": isLogged}));
-  });
+	res.writeHead(statusToSend, {"Content-Type": "application/json"});
+    res.end(JSON.stringify({"isLogged": isLogged, "id": idToSend}));
+  }
+  catch(err){
+	console.log(err);
+	res.writeHead(500, {"Content-Type": "application/json"});
+    res.end(JSON.stringify({"msg": "There was an unpredicted error with the application. Please try again later."}));
+  }
 }
 
-function setLocais(req, res){
-  const info = req.body.info;
-  const now = new Date();
+async function setLocais(req, res){
+  try{
+    const info = req.body.info;
+    const now = new Date();
   
-  mysql.createConnection(settings)
-  .then((pool) => {
-    connection = pool;
-    return connection.query(`INSERT INTO TbLocal (nome, descricao, link, longitude, latitude, pontos, dataCadastro) VALUES ('${info.nome}', '${info.descricao}', '${info.link}', '${info.longitude}', '${info.latitude}', 10, '${now}')`);
-  })
-  .then((results) => {
-    console.log(results);
+    connection = await mysql.createConnection(settings)
+
+    await connection.query(`INSERT INTO TbLocal (nome, descricao, link, longitude, latitude, pontos, dataCadastro) VALUES ('${info.nome}', '${info.descricao}', '${info.link}', '${info.longitude}', '${info.latitude}', 10, '${now}')`);
     //do other stuff maybe
     res.writeHead(200, {"Content-Type": "application/json"});
     res.end(JSON.stringify({"msg": "local inserido com sucesso?"}));
-  });
+  }
+  catch(err){
+	console.log(err);
+	res.writeHead(500, {"Content-Type": "application/json"});
+    res.end(JSON.stringify({"msg": "There was an unpredicted error with the application. Please try again later."}));
+  }
 }
 
-function setUser(req, res){
-  const info = req.body.info;
-  const now = new Date();
+async function setUser(req, res){
+  try{
+    const info = req.body;
+    const now = new Date();
   
-  if(info.senha !== info.senhaConfirmacao){
-    res.writeHead(400, {"Content-Type": "application/json"});
-    res.end(JSON.stringify({"msg": "senhas não conferem"}));
-    return null;
-  }
+    if(info.senha !== info.senhaConfirmacao){
+      res.writeHead(400, {"Content-Type": "application/json"});
+      res.end(JSON.stringify({"msg": "senhas não conferem"}));
+      return null;
+    }
   
-  mysql.createConnection(settings)
-  .then((pool) => {
-    connection = pool;
+    connection = await mysql.createConnection(settings)
+
     const senhaHasheada = hasher(info.senha);
-    return connection.query(`INSERT INTO TbUsuario (nome, sobreNome, email, senha, idNivelAcesso, qtdPontos, dataCadastro) VALUES ('${info.nome}', '${info.sobreNome}', '${info.email}', '${senhaHasheada}'), 1, 0, '${now}'`);
-  })
-  .then((results) => {
-    console.log(results);
+    await connection.query(`INSERT INTO TbUsuario (nome, sobreNome, email, senha, idNivelAcesso, qtdPontos, dataCadastro) VALUES ('${info.nome}', '${info.sobreNome}', '${info.email}', '${senhaHasheada}'), 1, 0, '${now}'`);
+
     //do other stuff maybe
     res.writeHead(200, {"Content-Type": "application/json"});
     res.end(JSON.stringify({"msg": "usuário inserido com sucesso?"}));
-  });
+  }
+  catch(err){
+	console.log(err);
+	res.writeHead(500, {"Content-Type": "application/json"});
+    res.end(JSON.stringify({"msg": "There was an unpredicted error with the application. Please try again later."}));
+  }
 }
 
-function updateLocais(req, res){
-  const info = req.body.info;
-  const update = `UPDATE TbLocal SET nome = '${info.nome}', descricao = '${info.descricao}', link = '${info.link}', longitude = '${info.longitude}', latitude = '${info.latitude}' WHERE idLocal = info.id`;
+async function updateLocais(req, res){
+  try{
+    const info = req.body;
+    const update = `UPDATE TbLocal SET nome = '${info.nome}', descricao = '${info.descricao}', link = '${info.link}', longitude = '${info.longitude}', latitude = '${info.latitude}' WHERE idLocal = info.id`;
   
-  mysql.createConnection(settings)
-  .then((pool) => {
-    connection = pool;
-    return connection.query(update);
-  })
-  .then((results) => {
-    console.log(results);
+    connection = await mysql.createConnection(settings)
+
+    await connection.query(update);
+
     //do other stuff maybe
     res.writeHead(200, {"Content-Type": "application/json"});
     res.end(JSON.stringify({"msg": "informações atualizadas com sucesso?"}));
-  });
+  }
+  catch(err){
+	console.log(err);
+	res.writeHead(500, {"Content-Type": "application/json"});
+    res.end(JSON.stringify({"msg": "There was an unpredicted error with the application. Please try again later."}));
+  }
 }
 
 function hasher(plainText=""){
